253efd2a6860cc68636fa843dfab280683524cd3
phunghaphung <haphungworks@gmail.com>
2025-10-05 20:27:48 -0400

    fe-13: add sponsorship under home

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import FeaturedEvent from "@/components/FeaturedEvent";
 import ContactSection from "@/components/ContactSection";
 import AboutSection from "@/components/AboutSection";
+import SponsorshipSection from "@/components/SponsorshipSection";
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
       <FeaturedEvent />
       <AboutSection />
       <ContactSection />
+      <SponsorshipSection />
     </>
   );
 }
